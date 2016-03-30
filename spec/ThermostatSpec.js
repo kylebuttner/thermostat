729cb2d7f2b3b1d724064b7f22d63900f2295eca
Kyle Buttner <kylebuttner@gmail.com>
2016-03-30 13:33:46 +0100

    add increase/decrease functions

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -6,6 +6,40 @@ describe('Thermostat',function(){
   });
 
   it('it should start at 20degrees',function(){
-    expect(thermostat.reading()).toEqual(20);
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+
+  it('should increase the temperature',function(){
+    thermostat.increaseTemperature();
+    expect(thermostat.getCurrentTemperature()).toBeGreaterThan(20);
+  });
+
+  it('should decrease the temperature',function(){
+    thermostat.decreaseTemperature();
+    expect(thermostat.getCurrentTemperature()).toBeLessThan(20);
+  });
+
+  it('should have a minimum temperature of 10 degreees',function(){
+    for (i=0;i<11;i++) {
+      thermostat.decreaseTemperature();
+    };
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
+  });
+
+  it('should have a power saving mode',function() {
+    expect(thermostat.powerSavingMode).toEqual(false);
+  });
+
+  describe('power saving mode is on',function() {
+    beforeEach(function() {
+      thermostat.powerSavingMode = true;
+    });
+
+    it('should have a maximum temperature of 25 degrees', function() {
+      for (i=0;i<6;i++) {
+        thermostat.increaseTemperature();
+      };
+      expect(thermostat.getCurrentTemperature()).toEqual(25)
+    });
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,7 +1,34 @@
 function Thermostat(){
- this._defaultTemperature = 20
-}
+ this.temperature = 20;
+ this.powerSavingMode = false;
+};
 
-Thermostat.prototype.reading = function () {
-return this._defaultTemperature;
+// Thermostat.prototype.reading = function () {
+// return this._defaultTemperature;
+// };
+
+Thermostat.prototype.increaseTemperature = function() {
+  if (this.powerSavingMode === true) {
+    if (this.temperature < 25) {
+      return this.temperature++;
+    } else {
+      return this.temperature;
+    };
+  } else {
+    return this.temperature++;
+  };
+
+};
+
+Thermostat.prototype.getCurrentTemperature = function() {
+  return this.temperature;
+};
+
+Thermostat.prototype.decreaseTemperature = function() {
+  if (this.temperature > 10) {
+    return this.temperature--;
+  }
+  else {
+    this.temperature;
+  };
 };
